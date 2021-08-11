2c8ff0907ddc1925a8a77fa9bc1c3227dc4f957e
gabo180 <gabrieldejesushernandez@gmail.com>
2021-08-11 04:22:00 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -4,6 +4,31 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.string(120), unique=True, nullable=False)
+    last_name = db.Column(db.string(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.first_name,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+# id int PK
+# first_name string
+# last_name string
+# email string
+# password string
+
+class (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -17,3 +42,9 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+# id int PK
+# user_id int
+# character_id int
+# vehicle_id  int
+# planet_id   int
